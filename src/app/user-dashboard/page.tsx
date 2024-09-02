20c9a6e02f79e75f9553b923971f68a364935842
Meraj <haquedot@gmail.com>
2024-09-03 03:26:04 +0530

    fixed some of the issues for deployment

--- src/app/user-dashboard/page.tsx
+++ src/app/user-dashboard/page.tsx
@@ -225,7 +225,7 @@ const UserDashboard: FC = () => {
         <Sidebar open={open} setOpen={setOpen} animate={true}>
           <SidebarBody className="justify-between gap-10">
             <div className="flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
-              <Logo />
+              {/* <Logo /> */}
               <div className="mt-8 flex flex-col gap-2">
                 {links.map((link, idx) => (
                   <SidebarLink
@@ -273,23 +273,23 @@ const UserDashboard: FC = () => {
   );
 };
 
-export const Logo = () => {
-  return (
-    <Link
-      href="/"
-      className="font-normal flex space-x-2 items-center text-sm text-black py-1 relative z-20"
-    >
-      <div className="h-5 w-6 bg-black rounded-br-lg rounded-tr-sm rounded-tl-lg rounded-bl-sm flex-shrink-0" />
-      <motion.span
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        className="font-medium text-black whitespace-pre"
-      >
-        Cariload
-      </motion.span>
-    </Link>
-  );
-};
+// const Logo = () => {
+//   return (
+//     <Link
+//       href="/"
+//       className="font-normal flex space-x-2 items-center text-sm text-black py-1 relative z-20"
+//     >
+//       <div className="h-5 w-6 bg-black rounded-br-lg rounded-tr-sm rounded-tl-lg rounded-bl-sm flex-shrink-0" />
+//       <motion.span
+//         initial={{ opacity: 0 }}
+//         animate={{ opacity: 1 }}
+//         className="font-medium text-black whitespace-pre"
+//       >
+//         Cariload
+//       </motion.span>
+//     </Link>
+//   );
+// };
 
 
 export default UserDashboard;
